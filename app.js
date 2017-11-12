03ccbe3165e6fe29ce3891b0b7a6fdcdff502253
Shrey Sudan <omegacodingin@gmail.com>
2017-11-13 01:43:10 +0530

    Logic of app completed, UI components left

--- app.js
+++ app.js
@@ -15,16 +15,24 @@ app.get('/', function(req, res){
 });
 
 app.get('/results', function(req, res){
+	//Search URL
 	var title = (req.query.title).replace(/ /g, "+");
 	var apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=793712055a84b83ce568eafcd712f272&query=";
 	var url = apiUrl + title;
-	/*console.log(url);*/
+	
+	//Backdrop Image url
+	var base_url = "http://image.tmdb.org/t/p/";
+	var size = "w185";
+
+	//HTTP request to API
 	request(url, function(err, response, body){
 		if(err){
 			console.log(err);
 		} else if(res.statusCode === 200){
 			var body = JSON.parse(body);
-			res.render("results", {body: body});
+			var img_url = base_url + size; /*body.results[0]["backdrop_path"]*/
+			/*console.log(img_url);*/
+			res.render("results", {body: body, img_url: img_url});
 		}
 	});
 });
